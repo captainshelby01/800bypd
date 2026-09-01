cbf25af5a8407d839310e1eb76c499342bd26c16
Saliu Oluwafemi <117808529+captainshelby01@users.noreply.github.com>
2026-09-01 18:26:21 +0100

    Fix SSL reverse proxy trusted proxies and force HTTPS scheme for Render deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,11 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\URL;
+
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    URL::forceScheme('https');
+}
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
         ]);
